7f50ae810460bba8be18d19fb71f1c42c40dd090
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-12-04 13:44:12 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BbGetPhysicalApp::Application.config.secret_token = '22a653cd9652f52ba8f449794fe268d3b5b8ec14aff9aa25f5c2dab0fac88510144ffc01ce53c530cd664b48f870b50c047456abdb053be255576a318b506dd7'
